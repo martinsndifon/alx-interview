d648a3da2a340cebe663123d9f9de2cf618e4ae1
Martins Ndifon <martinsndifon@gmail.com>
2023-07-04 23:21:31 +0000

    Initial solution of the locked boxes problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lockboxes challenge solution"""
+
+
+def canUnlockAll(boxes):
+    """Main logic"""
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+
+    def dfs(box):
+        """depth-first search algorithm"""
+        visited[box] = True
+        for key in boxes[box]:
+            if not visited[key]:
+                dfs(key)
+
+    dfs(0)
+
+    # check if all boxes were visited
+    return all(visited)
