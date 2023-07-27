b566bfb247768819ca2ed118890b1e0151247526
Martins Ndifon <martinsndifon@gmail.com>
2023-07-27 12:26:12 +0000

    made some corrections for pycodestyle compliance

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,14 @@
 #!/usr/bin/python3
-"""a module to test with"""
-
-def b22(j, data):
-    """a test info to test with"""
-    return j+1 < len(data) and data[j+1] > 127 \
-        and data[j+1] < 192
+"""UTF-8 validation module"""
 
 
 def validUTF8(data):
-    """another test info to test with"""
-    if isinstance(data, list) and len(data):
-        ln = len(data)
-        j = 0
-# redundant comment just to have a commit
-        while j < ln:
-            b1 = data[j] > 0 and data[j] < 128
-            b2 = data[j] > 191 and data[j] < 224 and b22(j, data)
-            b3 = data[j] > 223 and data[j] < 240 \
-                and b22(j, data) and b22(j+1, data)
-            b4 = data[j] > 239 and data[j] < 248 \
-                and b22(j, data) \
-                and b22(j+1, data) \
-                and b22(j+2, data)
-            if b1:
-                j += 1
-                continue
-            elif b2:
-                j += 2
-                continue
-            elif b3:
-                j += 3
-                continue
-            elif b4:
-                j += 4
-                continue
-            else:
-                return False
+    """Determines if the given data is a valid UTF-8 encoding"""
+    if any(val < 0 or val >= 256 for val in data):
+        return False
+
+    try:
+        decoded_text = bytes(data).decode('utf-8')
         return True
-    return False
+    except UnicodeDecodeError:
+        return False
