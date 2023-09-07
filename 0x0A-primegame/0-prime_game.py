f3bb473a9df126b765b9973acc2951f2acf88b75
Martins Ndifon <martinsndifon@gmail.com>
2023-09-07 18:19:29 +0100

    First attempt to solve the prime game problem

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""ALX SE Prime Game"""
+from typing import List, Union
+
+
+def prime_nums_in_range(start: int, end: int) -> List:
+    """Finds and returns all the prime numbers in a range"""
+    prime_numbers = []
+
+    for num in range(start, end + 1):
+        if num > 1:
+            is_prime = True
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                prime_numbers.append(num)
+
+    return prime_numbers
+
+
+def isWinner(x: int, nums: List) -> Union[str, None]:
+    """Returns the name of the winner that won the most rounds"""
+    maria, ben = 0, 0
+    for i in range(x):
+        result = prime_nums_in_range(1, nums[i])
+        if len(result) % 2:
+            maria += 1
+        else:
+            ben += 1
+    if maria > ben:
+        return 'Maria'
+    elif ben > maria:
+        return 'Ben'
+    else:
+        return None
